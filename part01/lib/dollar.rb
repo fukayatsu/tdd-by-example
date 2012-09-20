d9ff2d57dc527cf3a2b6cd2ba7a453df8a01e9f0
fukayatsu <fukayatsu@gmail.com>
2012-09-20 10:14:34 +0900

    pass example of p.4

--- part01/lib/dollar.rb
+++ part01/lib/dollar.rb
@@ -1,5 +1,12 @@
 class Dollar
+  def initialize(amount)
+  end
+
   def amount
     10
+  end
+
+  def multiply(multiplier)
+
   end
 end
--- part01/spec/dollar_spec.rb
+++ part01/spec/dollar_spec.rb
@@ -4,7 +4,8 @@ require 'dollar'
 
 describe Dollar do
   it 'can multiply' do
-    five = Dollar.new
+    five = Dollar.new 5
+    five.multiply 2
     five.amount.should == 10
   end
 end
