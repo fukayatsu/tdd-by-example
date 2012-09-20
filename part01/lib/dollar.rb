821613acc22047e969c98ad975d8e97385797056
fukayatsu <fukayatsu@gmail.com>
2012-09-20 10:09:15 +0900

    [from now] 2012/09/20 10:09:15

--- part01/lib/dollar.rb
+++ part01/lib/dollar.rb
@@ -1,2 +1,5 @@
 class Dollar
+  def amount
+    10
+  end
 end
--- part01/spec/dollar_spec.rb
+++ part01/spec/dollar_spec.rb
@@ -4,8 +4,7 @@ require 'dollar'
 
 describe Dollar do
   it 'can multiply' do
-    #five = Dollar.new 5
-    #five.times 2
+    five = Dollar.new
     five.amount.should == 10
   end
 end
