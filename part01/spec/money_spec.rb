921fe120d086e92f2a522059f3f26ee42adb6094
fukayatsu <fukayatsu@gmail.com>
2012-10-07 16:25:06 +0900

    modify spec

--- part01/spec/money_spec.rb
+++ part01/spec/money_spec.rb
@@ -3,29 +3,19 @@
 require 'money'
 
 describe Money do
-  describe '#dollar' do
-    it '#times' do
-      five = Money.dollar 5
-      (five.times 2).should == Money.dollar(10)
-      (five.times 3).should == Money.dollar(15)
-    end
-
-    it '#==' do
-      Money.dollar(5).should == Money.dollar(5)
-      Money.dollar(5).should_not == Money.dollar(6)
-    end
+  it '#times' do
+    five = Money.dollar 5;
+    five.times(2).should == Money.dollar(10)
+    five.times(3).should == Money.dollar(15)
   end
 
-  describe '#franc' do
-    it '#times' do
-      five = Money.franc 5
-      (five.times 2).should == Money.franc(10)
-      (five.times 3).should == Money.franc(15)
-    end
+  it '#==' do
+    Money.dollar(5).should == Money.dollar(5)
+    Money.dollar(5).should_not == Money.dollar(6)
 
-    it '#==' do
-      Money.franc(5).should == Money.franc(5)
-      Money.franc(5).should_not == Money.franc(6)
-    end
+    Money.franc(5).should == Money.franc(5)
+    Money.franc(5).should_not == Money.franc(6)
+
+    Money.dollar(5).should_not == Money.franc(5)
   end
 end
