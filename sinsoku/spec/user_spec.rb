b279893b576503fd53f907afb504c047ef4cd156
fukayatsu <fukayatsu@gmail.com>
2012-10-08 19:54:02 +0900

    refactor subject

--- sinsoku/spec/user_spec.rb
+++ sinsoku/spec/user_spec.rb
@@ -4,16 +4,12 @@ require 'user'
 describe User do
   describe '#admin?' do
     context '管理者の場合' do
-      before { @admin_user = User.new(role: 'admin') }
-
-      subject { @admin_user }
+      subject(:admin_user) { User.new(role: 'admin') }
       it { should be_admin }
     end
 
     context '一般ユーザの場合' do
-      before { @ordinary_user = User.new(role: nil) }
-
-      subject { @ordinary_user }
+      subject(:ordinary_user) { User.new(role: nil) }
       it { should_not be_admin }
     end
   end
